733b4e90169c8a58ccb1662e8bb87204ae27b140
Sam <webantelopestudio@gmail.com>
2019-05-25 00:02:33 +0300

    Add Swagger

---
+++ src/configuration/connection.swagger.ts
@@ -0,0 +1,15 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function connectSwagger(app) {
+  const swaggerSettings = new DocumentBuilder()
+    .setTitle('Service Authentication')
+    .setDescription('Authentication service for Booking house. Token JWT. Strategy Google, Facebook, Local.')
+    .setVersion('1.0.0')
+    .addTag('auth')
+    .setSchemes('http', 'https')
+    .build()
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerSettings);
+  SwaggerModule.setup('api/docs', app, swaggerDocument);
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { connectSwagger } from './configuration/connection.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  await connectSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
