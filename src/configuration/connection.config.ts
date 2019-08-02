b9c4bd867d67ce43487ee54d4006fcdd885ee393
Sam <webantelopestudio@gmail.com>
2019-08-02 15:56:18 +0300

    Upgrade config

---
+++ src/configuration/connection.config.ts
@@ -0,0 +1,5 @@
+import * as dotenv from 'dotenv';
+
+export function connectConfig() {
+  dotenv.config();
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { connectConfig } from './configuration/connection.config';
 import { connectSwagger } from './configuration/connection.swagger';
 import { connectValidator } from './configuration/connection.validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  await connectConfig();
   await connectSwagger(app);
   await connectValidator(app);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
